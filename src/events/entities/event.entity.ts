63096fed1b93fcb93e980749aab4646d92eaec25
grafamadeus <grafakranime@gmail.com>
2026-09-10 22:50:08 +0600

    add * entities

---
+++ src/events/entities/event.entity.ts
@@ -0,0 +1,43 @@
+import { Category } from 'src/categories/entities/categories.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+
+@Entity()
+export class Event {
+
+  @PrimaryGeneratedColumn()
+  id!: number; 
+
+  @Column()
+  title!: string;
+
+  @Column()
+  description!: string;
+
+  @Column()
+  date!: Date;
+
+  @Column()
+  address!: string;
+
+  @Column()
+  location!: string;
+
+  @Column({
+    type: 'decimal',
+  })
+  price!: number;
+
+  @Column()
+  capacity!: number;
+
+  @Column()
+  coverUrl!: string;
+
+  @ManyToOne(() => User)
+  user!: User;
+
+  @ManyToOne(()=>Category)
+  category!: Category;
+
+}
